f33e82c263ca62d032b88b0b185abc772310c84c
Satwik-Singh <61342456+Satwik-Singh@users.noreply.github.com>
2020-12-16 16:26:07 +0530

    initial commit
    added required files.

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+  $db_name = 'id15169349_ims_project';
+  $server = 'localhost';
+  $password = 'root';
+  $username = 'root';
+  $con = mysqli_connect($server, $username, $password, $db_name);
+ ?>
